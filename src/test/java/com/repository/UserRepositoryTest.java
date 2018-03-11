cc49bc0b2f1ace8365abcf84ecb4461b93dcda97
xbb123 <xbb123@naver.com>
2018-03-11 23:26:27 +0900

    Initial commit

---
+++ src/test/java/com/repository/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.repository;
+
+import com.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void name() {
+        assertEquals("hello", "hello");
+    }
+
+    @Test
+    public void save() {
+        User user = new User("dhshin", "1234", "daehoan");
+        userRepository.delete(user);
+        assertEquals(0, userRepository.count());
+        userRepository.save(user);
+        assertEquals(1, userRepository.count());
+    }
+}
